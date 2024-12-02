50c6f39b42c21e0fa707cebf1d41fe7df13bee9e
Roshith121 <roshith1213raj@gmail.com>
2024-12-03 00:33:01 +0530

    Updating code

--- ReverseAString.java
+++ ReverseAString.java
@@ -5,14 +5,36 @@ public class ReverseAString {
     public static void main(String[] args) {
     char[] s = {'h','e','l','l','o'};
         System.out.println(s);
-        System.out.println(reverseString(s));
+        char[] reverseArr = reverseString(s);
+        for(char ch:reverseArr){
+            System.out.print(ch);
+        }
+        System.out.println();
+        System.out.println("with new char variable ");
+        System.out.println(reverseStringGreedy(s));
     }
 
-    public static char[] reverseString(char[] s) {
+    //greedy algo
+    public static char[] reverseStringGreedy(char[] s) {
         String reverseS="";
         for(int i=s.length-1;i>=0;i--){
             reverseS+=s[i];
         }
         return reverseS.toCharArray();
+    }
+
+    //best approach
+    public static char[] reverseString(char[] s) {
+
+        int right = s.length-1;
+        int left = 0;
+        while(left<right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            right--;
+            left++;
+        }
+        return s;
     }
 }
