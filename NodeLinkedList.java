285eeacb6e3e14be46d1608da5aa32c853e38442
Roshith121 <roshith1213raj@gmail.com>
2024-12-04 11:48:26 +0530

    CommonLinkedList

---
+++ NodeLinkedList.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class NodeLinkedList {
+
+    int data;
+    NodeLinkedList next;
+
+    NodeLinkedList(int new_data){
+        this.data = new_data;
+        next=null;
+    }
+}
