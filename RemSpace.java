885b015ac16198b7a3b7ce0af534f002a70ea70a
Roshith121 <roshith1213raj@gmail.com>
2024-12-14 14:54:12 +0530

    removing space using reccursion

--- RemSpace.java
+++ RemSpace.java
@@ -1,10 +1,19 @@
 package org.example;
 
 public class RemSpace {
+    static String str = "";
     public static void main(String[] args) {
+        String inputStr ="I LOVE MY ROSHITH AND INDIA";
+        System.out.println(removeSpace(inputStr));
+    }
+    private static String removeSpace(String inputStr) {
 
-        String str= "I LOVE INDIA";
-        String newStr = str.replaceAll(" ","");
-        System.out.println(newStr);
+        if(inputStr.length()==0){
+            return "";
+        }
+        if(inputStr.charAt(0)==' '){
+            return removeSpace(inputStr.substring(1));
+        }
+       return inputStr.charAt(0) + removeSpace(inputStr.substring(1));
     }
 }
