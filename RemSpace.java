10b09041cf3c829e7b20b1046cba6ce4abfbca83
Roshith121 <roshith1213raj@gmail.com>
2024-12-14 13:22:33 +0530

    Removing space easy soln

---
+++ RemSpace.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class RemSpace {
+    public static void main(String[] args) {
+
+        String str= "I LOVE INDIA";
+        String newStr = str.replaceAll(" ","");
+        System.out.println(newStr);
+    }
+}
